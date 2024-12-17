a11c1fdbbf22c80de70cb0e8e0db0fab9693a2a0
hank <hank.shi@chisalsoft.com>
2024-12-17 18:29:01 +0800

    将package文件夹中文件移动到上层，解决导入自定义SPM库报错找不到Package.swift问题

--- Sources/LoggerLibrary/LoggerLibrary.swift
+++ Sources/LoggerLibrary/LoggerLibrary.swift
@@ -7,6 +7,7 @@
  2、终端 -- cd 本地某文件夹 -- git clone https://xxx仓库.xxx.git（HTTPS路径）
  3、在刚才克隆到本地的文件夹（xxx仓库）中创建 自定义 package
  4、在 git add. git commit 之后  git push origin main(可能是其他分支名)，将本地package提交到远程仓库
+ 补充：第3点中 Package.swift 必须位于该文件夹的第一层（即将 package文件夹中的所有文件转移到 xxx仓库文件夹中）
  */
 
 public struct LoggerLibrary {

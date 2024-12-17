db1d3e81c42d0f0c3ac6e118f34c4c8d011952f6
hank <hank.shi@chisalsoft.com>
2024-12-17 18:13:36 +0800

    1、添加一些提交步骤记录

--- LoggerLibrary/Sources/LoggerLibrary/LoggerLibrary.swift
+++ LoggerLibrary/Sources/LoggerLibrary/LoggerLibrary.swift
@@ -1,6 +1,14 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+/**
+ 远程仓库：
+ 1、先在GitHub个人仓库中创建一个 xxx名称 的仓库
+ 2、终端 -- cd 本地某文件夹 -- git clone https://xxx仓库.xxx.git（HTTPS路径）
+ 3、在刚才克隆到本地的文件夹（xxx仓库）中创建 自定义 package
+ 4、在 git add. git commit 之后  git push origin main(可能是其他分支名)，将本地package提交到远程仓库
+ */
+
 public struct LoggerLibrary {
     public enum LogLevel: String {
         case normal = "Logger-normal"
